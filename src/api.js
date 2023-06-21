adf22f2fbc85e2c3aaecd95a7bdf639f283e3068
Kfmoi <faragk1@mcmaster.ca>
2023-06-20 22:58:11 -0400

    Changed API Keys

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "a81d9f1fb7msh0a1fb45e5c97643p154f8bjsn553577b18c93",
+    "X-RapidAPI-Key": "YOUR_API_KEY",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -10,4 +10,4 @@ export const geoApiUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const weatherApiUrl = "https://api.openweathermap.org/data/2.5";
 
-export const weatherApiKey = "eddd83c8e0964752ec3d4713e5467e4d";
+export const weatherApiKey = "YOUR_API_KEY";
